b8327ef4476625d1361a784e58a87f74d20ae2f0
lndsilva <lndsilva@hotmail.com>
2023-11-27 21:58:22 -0300

    adapter recipes

--- app/src/main/java/br/com/etecia/myapp/AdapterRecipe.java
+++ app/src/main/java/br/com/etecia/myapp/AdapterRecipe.java
@@ -1,8 +1,11 @@
 package br.com.etecia.myapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,11 +19,18 @@ public class AdapterRecipe extends RecyclerView.Adapter<AdapterRecipe.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        view = layoutInflater.inflate(R.layout.modelo_recipes, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtModeloRecipe.setText(lstFilmes.get(position).getTitulo());
+        holder.imgModeloRecipe.setImageResource(lstFilmes.get(position).getImgFilmes());
 
     }
 
@@ -30,9 +40,13 @@ public class AdapterRecipe extends RecyclerView.Adapter<AdapterRecipe.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgModeloRecipe;
+        TextView txtModeloRecipe;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgModeloRecipe = itemView.findViewById(R.id.imgModeloRecipe);
+            txtModeloRecipe = itemView.findViewById(R.id.txtModeloRecipe);
         }
     }
 }
